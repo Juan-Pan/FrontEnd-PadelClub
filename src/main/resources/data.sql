aa57a0ff14f7d3cded51448dc7e2a8428097c03d
JorgeMartinMarina <jorgemartinmarina@gmail.com>
2026-05-15 16:05:05 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,29 @@
 merge into rol (id_rol, nombre_rol, descripcion) key (id_rol)
-values (1, 'USER', 'Usuario con permisos basicos');
+values (1, 'USER', 'Usuario estándar');
 
 merge into rol (id_rol, nombre_rol, descripcion) key (id_rol)
-values (2, 'ADMIN', 'Usuario con permisos administrativos');
+values (2, 'ADMIN', 'Administrador');
+
+merge into usuario
+    (nombre, apellidos, email, password, telefono, activo, fecha_registro, id_rol)
+key (email)
+values
+    ('Carlos', 'Alcaraz', 'carlos@padel.com', '{noop}Password123!', '600123456', true, NOW(), 1);
+
+merge into usuario
+    (nombre, apellidos, email, password, telefono, activo, fecha_registro, id_rol)
+key (email)
+values
+    ('Ana', 'Admin', 'ana.admin@padel.com', '{noop}123456', '600000001', true, NOW(), 2);
+
+merge into usuario
+    (nombre, apellidos, email, password, telefono, activo, fecha_registro, id_rol)
+key (email)
+values
+    ('Usuario', 'Prueba', 'user@padel.com', '{noop}1234', '600000002', true, NOW(), 1);
+
+merge into usuario
+    (nombre, apellidos, email, password, telefono, activo, fecha_registro, id_rol)
+key (email)
+values
+    ('Admin', 'Prueba', 'admin@padel.com', '{noop}1234', '600000003', true, NOW(), 2);
